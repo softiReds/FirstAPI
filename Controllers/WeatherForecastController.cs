374679827f3b34fc0f13f93a3df50e5477c751d4
Santiago Rojas <santirojastr12@gmail.com>
2023-08-15 07:22:36 -0500

    Manejo de rutas

--- Controllers/WeatherForecastController.cs
+++ Controllers/WeatherForecastController.cs
@@ -5,7 +5,11 @@ using Microsoft.AspNetCore.Mvc;
 namespace WebApi.Controllers;
 
 [ApiController]
-[Route("[controller]")] // La ruta especifica como se debe consumir el controlador, en este caso, el nombre del controlador será la forma mediante la cual accederemos a los endpoints del controlador
+[Route("api/[controller]")]     // Enrutamiento a nivel del controlador
+// La ruta especifica como se debe consumir el controlador, en este caso, el nombre del controlador será la forma mediante la cual accederemos a los endpoints del controlador
+// "[controller]" -> Permite que se maneje una ruta dinamica, en este caso se maneja el nombre del controladol Sin necesidad de especificar el nombre del controlador, podremos acceder al controlador mediante el nombre (independientemente de los cambios que se hhagan en el nombre, la ruta dinamica toma los cambios automaticamente)
+// "[parameter]" -> Parametro dinamico
+
 public class WeatherForecastController : ControllerBase
 {
     private static readonly string[] Summaries = new[]
@@ -45,6 +49,11 @@ public class WeatherForecastController : ControllerBase
     */
 
     [HttpGet(Name = "GetWeatherForecast")]
+    [Route("Get/weatherforecast")]  // Ruta a nivel de request
+    // La ruta para aceder a este metodo en especifico debemos hacer uso de la ruta a nivel de controlador y la ruta a nivel del request (metodo) -> servidor/api/[controller]/Get/weatherforecast
+    [Route("Get/weatherforecast2")] // Podemos asignar mas de una ruta por request, el metodo se ejecutará con cualquiera de las rutas configuradas
+    [Route("[action]")] // [action] -> Permite que se utilice el nombre del metodo para hacer el llamado del endpoint
+    // [Route("[action]/weatherforecast")]  -> Ruta similar a la que se encuentra en la linea 51, utiliza el nombre dle metodo ("Get") y otra palabra ("weatherforecast")
     public IEnumerable<WeatherForecast> Get()
     {
         return ListWeatherForecast;
